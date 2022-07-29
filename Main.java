8a76bcb9284f5ab65a0e8f8b5247296d5174d501
Mario280024 <110250420+Mario280024@users.noreply.github.com>
2022-07-29 12:22:19 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Yuvraj");
+    System.out.println("Hello Harshita");
+    System.out.println("*      *"); 
+    System.out.println(" *   *");
+    System.out.println("   * ");
+    System.out.println("   * ");
+    System.out.println("   * ");
+    System.out.println("   * ");
+
+
+    
+    System.out.println("*      *");
+    System.out.println("*      *");
+    System.out.println("*      *");
+    System.out.println("*      *");
+    System.out.println("*      *");
+    System.out.println("********");
+
+
+    
+    System.out.println("@      #");
+    System.out.println(" %   &");
+    System.out.println("  $ @");
+  }
+}

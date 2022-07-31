8dd755fb7e69b19706253ac84d0c15bd79e8855d
Mario280024 <110250420+Mario280024@users.noreply.github.com>
2022-07-31 13:54:37 -0400

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    // I am writing a paragraph from the book Harry Potter and the Sorcerer's Stone. This is the first paragraph from Chapter 2.
+    String S1 = "Nearly ten years had passed since the Dursleys had woken up to find their nephew on the front step, but Privet Drive had hardly changed at all.";
+    System.out.println(S1);
+     System.out.println();
+    System.out.println("The sun rose on the same tidy front gardens and lit up the brass number four on the Dursleys' front door; it crept into their living room, which was almost exactly the same as it had been on the night when Mr. Dursley had seen that fateful news report about the owls.");
+     System.out.println();
+    System.out.println("Only the photographs on the mantelpiece really showed how much time had passed.");
+    System.out.println(); 
+    System.out.println("Ten years ago, there had been lots of pictures of what looked like a large pink beach ball wearing different-colored bonnets - but Dudley Dursley was no longer a baby, and now the photographs showed a large blond boy riding his first bicycle, on a carousel at the fair, playing a computer game with his father, being hugged and kissed by his mother.");
+    System.out.println();
+    System.out.println("The room held no sign at all that another boy lived in the house, too.");
+  }
+}
